68090574f74a245c9717b3648092408038eecca8
benshabbat <benshabbat27@gmail.com>
2023-06-07 23:11:24 +0300

    add moment for reviews

--- src/components/landing/reviews/Review.jsx
+++ src/components/landing/reviews/Review.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Rating } from "@mui/material";
+import moment from "moment";
 const Review = ({ customer, index }) => {
   return (
     <div className="one-review" key={index}>
       <h1>{customer.name}</h1>
       <Rating value={customer.stars} readOnly />
       <div className="desc">{customer.description}</div>
+      <div className="desc">{moment(customer.updatedAt).startOf("ss").fromNow()}</div>
     </div>
   );
 };
