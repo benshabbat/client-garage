d63f75647560a16cf9817adf2061addfb735963d
benshabbat <benshabbat27@gmail.com>
2023-07-13 20:21:20 +0300

    fix valid car in edit car

--- src/components/edit/EditCar.jsx
+++ src/components/edit/EditCar.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Form, OpenModel } from "../index";
 import { updateCar } from "../../Utils";
-import {ValidCar} from "../../validation/Valid"
+import { validCar } from "../../validation/Valid";
 const EditCar = ({ handelClick, isOpen, car }) => {
   const [formData, setFormData] = useState(car);
-  // const [isValidCar, setIsValidCar] = useValidCar();
+  const [isValidCar, setIsValidCar] = useState();
   const onSubmit = async (e) => {
     e.preventDefault();
-    // isValidCar
-    //   ? await updateCar(car?._id, formData)
-    //   : console.log("ERROR");
+    isValidCar ? await updateCar(car?._id, formData) : console.log("ERROR");
 
     handelClick();
   };
-  // setIsValidCar(formData);
-
+  useEffect(() => {
+    setIsValidCar(validCar(formData?.numberPlate));
+  }, [formData?.numberPlate]);
   return (
     <OpenModel
       comp={
@@ -27,9 +26,10 @@ const EditCar = ({ handelClick, isOpen, car }) => {
               name: "numberPlate",
               type: "text",
               value: formData?.numberPlate,
-              pattern:
-                "[0-9]{3}[-][0-9]{2}[-][0-9]{3}|[0-9]{2}[-][0-9]{3}[-][0-9]{2}|[0-9]{7,8}",
               title: "Number of car must 00-000-00 OR 000-00-000",
+              errorMessage: "Your Car numer is wrong",
+              isError: !isValidCar,
+              invalid: isValidCar,
             },
             {
               name: "km",
